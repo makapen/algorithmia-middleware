a776dde44105bd84487f610ef3631bc290067fe6
tyleriguchi <tyleriguchi@gmail.com>
2015-09-24 10:37:26 -0700

    use data from client for sentiment

--- routes/sentiment.js
+++ routes/sentiment.js
@@ -3,7 +3,6 @@ var router = express.Router();
 var request = require('request');
 
 router.post('/', function(req, res) {
-  console.log('process.env.algorithmiaKey', req.body)
   request({
     url:' https://api.algorithmia.com/v1/algo/nlp/SentimentAnalysis/0.1.1',
     method: 'POST',
@@ -12,12 +11,11 @@ router.post('/', function(req, res) {
       'Authorization': process.env.algorithmiaKey
     },
     json: true,
-    body: JSON.stringify('bottom ten reasons you hate your job'),
+    body: JSON.stringify(req.body),
   }, function(err, returned, body) {
     if (err) {
       console.log('er', err)
     }
-    console.log('returned', body)
     res.status(200).send(body)
   })
 });
